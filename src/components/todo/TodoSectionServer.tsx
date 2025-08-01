9bc61a6367775dc2b01d6862ca66ce510712b8eb
Lewis_Kett <lewis.a.kett@gmail.com>
2025-08-01 14:40:18 +0100

    adds suspense to async comps

--- src/components/todo/TodoSectionServer.tsx
+++ src/components/todo/TodoSectionServer.tsx
@@ -1,19 +1,20 @@
-import { getTodos, getTodoStats } from '@/actions/todo-actions'
+import { Suspense } from 'react'
+import { getTodos } from '@/actions/todo-actions'
 import { TodoFormClient } from './TodoFormClient'
 import { TodoList } from './TodoList'
 import { TodoStats } from './TodoStats'
 
 export async function TodoSectionServer() {
-  const [todos, stats] = await Promise.all([
-    getTodos(),
-    getTodoStats()
-  ])
+  const todos = await getTodos()
+  const pendingCount = todos.filter(todo => !todo.completed).length
 
   return (
     <div className="space-y-6">
       <TodoFormClient />
-      <TodoList todos={todos} pendingCount={stats.pendingCount} />
-      <TodoStats />
+      <TodoList todos={todos} pendingCount={pendingCount} />
+      <Suspense fallback={<div className="text-center text-sm text-muted-foreground">Loading stats...</div>}>
+        <TodoStats />
+      </Suspense>
     </div>
   )
 }
